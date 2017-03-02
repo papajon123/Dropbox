2728f252a3a4a5d12f7b6505e3b304a8bea0f8fc
papajon123 <nos_05@mail.ru>
2017-03-02 20:11:05 +0300

    this tamogochi!

---
+++ VS projext/WpfApplication1/WpfApplication1/Classes/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    abstract class Animal
+    {
+        protected Animal(int heal, int takeEat, int takeCrap, int takeFunny)
+        {
+            this._heal = heal;
+            this._takeEat = takeEat;
+            this._takeCrap = takeCrap;
+            this._takeFunny = takeFunny;
+        }
+
+        private int _heal;
+
+        private int _takeEat;
+
+        private int _takeCrap;
+
+        private int _takeFunny;
+    }
+}
